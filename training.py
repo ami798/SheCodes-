bfbac1b246cd5f93fd4c25007064fee6ec392756
Amira Abdurahman <amiraabdurahman8@gmail.com>
2025-01-24 11:13:11 +0300

    Update training.py

--- training.py
+++ training.py
@@ -1,14 +1,1 @@
-def get_username():
-    while True:
-        username = input("Please enter your name: ").strip()
-        if username == "":
-            print("You didn't enter a name. Please try again.")
-        else:
-            confirmation = input(f"Is this your name? {username} (yes/no): ").strip().lower()
-            
-            if confirmation == "yes":
-                print("Complete!")
-                
-# Start the process
-get_username()
- 
+
